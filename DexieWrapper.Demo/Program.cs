d9487aedd8e661bdc90362b273a1fa12400db095
Simon Kuster <95976755+Kustersi@users.noreply.github.com>
2023-02-28 21:29:17 +0100

    Add method AddDexieWrapper to register DI

--- DexieWrapper.Demo/Program.cs
+++ DexieWrapper.Demo/Program.cs
@@ -3,17 +3,15 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Nosthy.Blazor.DexieWrapper.Demo;
 using Nosthy.Blazor.DexieWrapper.Demo.Database;
 using Nosthy.Blazor.DexieWrapper.Demo.Persons;
-using Nosthy.Blazor.DexieWrapper.JsModule;
-using Nosthy.Blazor.DexieWrapper.ObjUrl;
+using Nosthy.Blazor.DexieWrapper.Extensions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<IModuleFactory, EsModuleFactory>();
+builder.Services.AddDexieWrapper();
 builder.Services.AddScoped<MyDb>();
-builder.Services.AddScoped<ObjectUrlService>();
 builder.Services.AddScoped<PersonRepository>();
 
 await builder.Build().RunAsync();
---
+++ DexieWrapper/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Nosthy.Blazor.DexieWrapper.Database;
+using Nosthy.Blazor.DexieWrapper.JsModule;
+using Nosthy.Blazor.DexieWrapper.ObjUrl;
+
+namespace Nosthy.Blazor.DexieWrapper.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddDexieWrapper(this IServiceCollection services)
+        {
+            services.AddScoped<IModuleFactory, EsModuleFactory>();
+            services.AddScoped<ObjectUrlService>();
+            services.AddScoped<Dexie>();
+        }
+    }
+}
