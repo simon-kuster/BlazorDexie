442463104f32586c71fe09a43bf7a5dd97fde137
Simon Kuster <95976755+Kustersi@users.noreply.github.com>
2023-02-28 21:45:09 +0100

    Extend AddDexieWrapper method for the use with custom IModuleFactory implementations

--- DexieWrapper.Test/Startup.cs
+++ DexieWrapper.Test/Startup.cs
@@ -1,9 +1,7 @@
 ﻿using Jering.Javascript.NodeJS;
 using Microsoft.Extensions.DependencyInjection;
-using Nosthy.Blazor.DexieWrapper.JsModule;
-using Nosthy.Blazor.DexieWrapper.ObjUrl;
 using Nosthy.Blazor.DexieWrapper.Test.ModuleWrappers;
-using Nosthy.Blazor.DexieWrapper.Database;
+using Nosthy.Blazor.DexieWrapper.Extensions;
 
 namespace DexieWrapper.Test
 {
@@ -12,13 +10,7 @@ namespace DexieWrapper.Test
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddNodeJS();
-           
-            services.AddTransient<IModuleFactory>(provider =>
-              new CommonJsModuleFactory(provider.GetRequiredService<INodeJSService>(), "../../../DexieWrapper/wwwroot"));
-
-            services.AddTransient(provider => new ObjectUrlService(provider.GetRequiredService<IModuleFactory>()));
-
-            services.AddTransient(provider => new Dexie(provider.GetRequiredService<IModuleFactory>()));
+            services.AddDexieWrapper(p => new CommonJsModuleFactory(p.GetRequiredService<INodeJSService>(), "../../../DexieWrapper/wwwroot"));
         }
     }
 }
--- DexieWrapper/Extensions/ServiceCollectionExtensions.cs
+++ DexieWrapper/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
 using Nosthy.Blazor.DexieWrapper.Database;
 using Nosthy.Blazor.DexieWrapper.JsModule;
 using Nosthy.Blazor.DexieWrapper.ObjUrl;
@@ -9,7 +10,12 @@ namespace Nosthy.Blazor.DexieWrapper.Extensions
     {
         public static void AddDexieWrapper(this IServiceCollection services)
         {
-            services.AddScoped<IModuleFactory, EsModuleFactory>();
+            AddDexieWrapper(services, p => new EsModuleFactory(p.GetRequiredService<IJSRuntime>()));
+        }
+
+        public static void AddDexieWrapper(this IServiceCollection services, Func<IServiceProvider, IModuleFactory> moduleFactoryFactory)
+        {
+            services.AddScoped<IModuleFactory>(p => moduleFactoryFactory(p));
             services.AddScoped<ObjectUrlService>();
             services.AddScoped<Dexie>();
         }
