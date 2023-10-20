b54574755722055258c080c9ca39cc63b734420c
Simon Kuster <95976755+Kustersi@users.noreply.github.com>
2023-10-20 18:39:14 +0200

    EsModule: Cancel operations if module is disposed

--- BlazorDexie/JsModule/EsModule.cs
+++ BlazorDexie/JsModule/EsModule.cs
@@ -5,37 +5,50 @@ namespace BlazorDexie.JsModule
     public sealed class EsModule : IModule
     {
         private Lazy<Task<IJSObjectReference>>? _jsObjectReferenceTask;
+        private CancellationTokenSource _internalCancellationTokenSource = new CancellationTokenSource();
+        private CancellationToken _internalCancellationToken;
+
 
         public EsModule(IJSRuntime jsRuntime, string modulePath)
         {
+            _internalCancellationToken = _internalCancellationTokenSource.Token;
             _jsObjectReferenceTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>("import", modulePath).AsTask());
         }
 
         public async Task<T> InvokeAsync<T>(string identifier, CancellationToken cancellationToken, params object[] args)
         {
-            var jsObjectReference = await GetJsObjectReference();
-            return await jsObjectReference.InvokeAsync<T>(identifier, cancellationToken, args);
+            var combinedCancellationToken = CancellationTokenSource.CreateLinkedTokenSource(_internalCancellationToken, cancellationToken).Token;
+            var jsObjectReference = await GetJsObjectReference(combinedCancellationToken);
+            return await jsObjectReference.InvokeAsync<T>(identifier, combinedCancellationToken, args);
         }
 
         public async Task InvokeVoidAsync(string identifier, CancellationToken cancellationToken, params object[] args)
         {
-            var jsObjectReference = await GetJsObjectReference();
-            await jsObjectReference.InvokeVoidAsync(identifier, cancellationToken, args);
+            var combinedCancellationToken = CancellationTokenSource.CreateLinkedTokenSource(_internalCancellationToken, cancellationToken).Token;
+            var jsObjectReference = await GetJsObjectReference(combinedCancellationToken);
+            await jsObjectReference.InvokeVoidAsync(identifier, combinedCancellationToken, args);
         }
 
         public async ValueTask DisposeAsync()
         {
-            if (_jsObjectReferenceTask?.IsValueCreated == true)
+            if (_jsObjectReferenceTask?.IsValueCreated == true && !_internalCancellationToken.IsCancellationRequested)
             {
-                var jsObjectReference = await GetJsObjectReference();
+                var jsObjectReference = await GetJsObjectReference(_internalCancellationToken);
 
                 await jsObjectReference.DisposeAsync().ConfigureAwait(false);
                 _jsObjectReferenceTask = null;
             }
+
+            _internalCancellationTokenSource.Cancel();
         }
 
-        private async Task<IJSObjectReference> GetJsObjectReference()
+        private async Task<IJSObjectReference> GetJsObjectReference(CancellationToken cancellationToken)
         {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                throw new OperationCanceledException();
+            }
+
             if (_jsObjectReferenceTask == null)
             {
                 throw new ObjectDisposedException("JsObjectReference is disposed");
